c23441950bf564b64fcf0a98fc6651a76c2f476d
Anna <annaersh1994@gmail.com>
2021-12-20 21:51:56 +0300

    Initial Commit

---
+++ SU02/SU02App.swift
@@ -0,0 +1,17 @@
+//
+//  SU02App.swift
+//  SU02
+//
+//  Created by Anna Ershova on 20.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SU02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
